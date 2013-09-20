0142824951284412e286ba351efe9d3f9b018322
Simon Kirkby <zignig@bl3dr.com>
2013-09-20 09:27:08 +0800

    data load and save

--- dht.py
+++ dht.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python -i
 
 from pydht import DHT 
+import json,uuid
+
+
+try:
+    os.stat('node_id.txt')
+    node_id = json.loads((open('node_id.txt').read()))
+except:
+    node_id = uuid.uuid4().int
+    f = open('node_id.txt','w')
+    f.write(json.dumps(node_id))
+    f.close()
 
 host,port = '',7000
 strap = 'bl3dr.com'
-d = DHT(host,port,boot_host=strap,boot_port=port)
+d = DHT(host,port,id=node_id,boot_host=strap,boot_port=port)
 nodes = d.iterative_find_nodes(2)
 print nodes
 
+def load_data(d):
+    " python json turns int keys into strings"
+    dodj_data = json.loads(open('data.txt').read())
+    k = dodj_data.keys()
+    data = {}
+    for i in k:
+        data[int(i)] = dodj_data[i]
+    d.data = data
+
+load_data(d)
 glob = {}
-try: 
-	d['server']
-	print d['server']
-except:
-	d['server'] = 'http://couch.bl3dr.com/zignig'
 
 def add(key,value):
 	glob[key] = 1
 	d['glob'] = glob.keys()
 	d[key] = value
+
+def save():
+    f = open('data.txt','w')
+    f.write(json.dumps(d.data))
+    f.close()
+
