c7ca346455f41f2fcdba6fc1c02276bb325d3321
Simon Kirkby <zignig@bl3dr.com>
2013-09-22 23:59:16 +0800

    increased loggin

--- test.py
+++ test.py
@@ -6,8 +6,13 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+
 logger.addHandler(ch)
 
 reg = register.registration()
