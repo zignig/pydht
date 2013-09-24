31a87e380767603680398f670cd1839bfe928bc3
Simon Kirkby <zignig@bl3dr.com>
2013-09-24 10:24:27 +0800

    remove old register test

--- dht.py
+++ dht.py
@@ -30,8 +30,6 @@ reg = register.registration()
 host,port = '',7000 
 strap = 'bl3dr.com'
 d = DHT(host,port,reg,id=reg.node_id,boot_host=strap,boot_port=port)
-#post the public key up by it's own hash
-d[str(reg.node_id)] = reg.pub.as_pem()
 
 
 nodes = d.iterative_find_nodes(2)
